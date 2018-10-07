8e1ce6e3d4ced8483e9d6309871c95522972d0ed
heejeongroh <heejeongroh@218.38.137.28>
2018-10-07 18:12:35 +0900

    baekjoon3671

---
+++ codeplus/src/codeplus/baekjoon3671.java
@@ -0,0 +1,27 @@
+package codeplus;
+
+import java.io.BufferedReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+/**
+ * baekjoon 3671s
+ * @author heejeongroh
+ *
+ */
+public class baekjoon3671 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		StringTokenizer st;
+		
+		int arr[];
+		
+		int c = scan.nextInt();
+		for(int i=0; i<c; i++) {
+			
+		}
+	}
+
+}
