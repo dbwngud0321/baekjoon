c3e3f287d6d5df29c50f76cbaf325bb4ad7bb7ae
heejeongroh <heejeongroh@192.168.0.4>
2018-10-09 20:03:18 +0900

    baekjoon2920 developing

--- codeplus/src/codeplus/baekjoon2920.java
+++ codeplus/src/codeplus/baekjoon2920.java
@@ -1,10 +1,27 @@
 package codeplus;
 
+import java.util.Scanner;
+
 public class baekjoon2920 {
 
+	static int N=8;
+	static int []arr;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		arr = new int[N];
+		for(int i=0; i<N; i++) {
+			arr[i] = scan.nextInt();
+		}
 
+		for(int i=0; i<N; i++) {
+			if(arr[i]!=1 || arr[i]!=8) {
+				System.out.println("mixed");
+			}else if(arr[i]==1){
+				
+			}else if(arr[i]==8) {
+				
+			}
+		}
 	}
 
 }
