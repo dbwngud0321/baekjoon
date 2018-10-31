7fa17f62c8b29c2187b6591a6cec371c03d1d12f
jh11.yoo <jh11.yoo@18A1D-0057-PC.secui.com>
2018-10-31 18:50:53 +0900

    dfs문제 추가

---
+++ codeplus/src/codeplus/DFS_baekjoon2606.java
@@ -0,0 +1,9 @@
+package codeplus;
+
+public class DFS_baekjoon2606 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ codeplus/src/codeplus/baekjoon8958.java
@@ -0,0 +1,18 @@
+package codeplus;
+
+import java.util.Scanner;
+
+public class baekjoon8958 {
+	
+	public static int N;
+	public static String str[];
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		N = scan.nextInt();
+		str = new String[N];
+		for(int i=0; i<N; i++){
+			
+		}
+	}
+}
