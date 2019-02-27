8fb6374b3170ccd2e6cad6d80f8c68d3ec70983f
ls <jh11.yoo@secui.com>
2019-02-27 15:37:01 +0900

    codility OddOccurrencesInArray commit

---
+++ codeplus/src/codility/OddOccurrencesInArray.java
@@ -0,0 +1,32 @@
+package codility;
+
+import java.util.Scanner;
+
+public class OddOccurrencesInArray {
+
+	public static int solution(int[] A) {
+		int result = 0;
+		int count = 0;
+		for(int i=0; i<A.length; i++){
+			for(int j=0; j<i; j++){
+				if(A[i] == A[j]){
+					count++;
+				}
+			}
+			if(count == 0){
+				result = A[i];
+			} else {
+				count = 0;
+			}
+		}
+		
+		
+		return result;
+	}
+	public static void main(String[] args) {
+		int A[] = {9,3,9,3,9,7,9};
+		System.out.println(solution(A));
+
+	}
+
+}
