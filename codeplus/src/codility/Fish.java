57fa9c2b2e60fd5f663771f6b635b468be8c0356
ls <jh11.yoo@secui.com>
2019-02-28 16:52:56 +0900

    Fish stack first commit

---
+++ codeplus/src/codility/Fish.java
@@ -0,0 +1,20 @@
+package codility;
+
+import java.util.Stack;
+
+public class Fish {
+	
+	public static int solution(int[] A, int[] B){
+		int result = 0;
+		Stack<Integer> stack = new Stack<Integer>();
+		
+		return result;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] A = {4,3,2,1,5};
+		int[] B = {0,1,0,0,0};
+		System.out.println(solution(A,B));
+	}
+
+}
